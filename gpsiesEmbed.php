8928e956fa255043dcb72d84355b34507e2801cc
flavio78vi <flavio78vi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-08 08:23:33 +0000

    rel 0.2
    git-svn-id: https://plugins.svn.wordpress.org/gpsiesembed/trunk@132935 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gpsiesEmbed.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: GPSies Embed
+Version: 0.1
+Plugin URI: http://flavio.alicubi.net/gpsies-embded/
+Description: Add GPSies Maps to your posts and pages.(Only for WordPress 2.5+)
+Author: flavio
+Author URI: http://flavio.alicubi.net/
+
+The tag to insert the code is: <code>[gpsies url width height]</code>
+
+License:
+ Released under the GPL license
+  http://www.gnu.org/copyleft/gpl.html
+
+  Copyright 2009 - 2010 flavioalberti (email : flavio@alicubi.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+define('GPS_EMBED_API_URL',		'http://www.gpsies.com/api.do?');
+define('GPS_EMBED_SHORTCODE',	'gpsies');
+define('GPS_EMBED_FILEID',		'fileId');
+define('GPS_EMBED_USERNAME',	'username');
+define('GPS_EMBED_CITY',		'city');
+define('GPS_EMBED_COUNTRY',		'country');
+define('GPS_EMBED_TRACKTYPE',	'trackTypes');
+define('GPS_EMBED_FILETYPE',	'filetype');
+define('GPS_EMBED_ZIP',			'zip');
+define('GPS_EMBED_BBOX',		'BBOX');
+define('GPS_EMBED_SEARCHTEXT',	'searchText');
+
+
+
+
+include (dirname (__FILE__).'/plugin.php');
+
+class GPSiesEmbed extends GPSiesEmbed_Plugin
+{
+	function GPSiesEmbed ()
+	{
+		$this->register_plugin ('gpsiesEmbed', __FILE__);
+		
+		$this->add_filter ('the_content');
+		$this->add_action ('wp_head');
+	}
+	
+	function wp_head ()
+	{
+		
+	}
+	
+	function replace ($matches)
+	{
+		$showmode = $matches[2];
+		$xmlresponse = $this->enquiry($matches[1]);
+		$parms =  array ('gpsies' => $xmlresponse, 'showmode'=>$showmode);
+		return $this->capture ('trackInfo',$parms);
+	}
+
+	function the_content ($text)
+	{
+		$result = preg_replace_callback ("@(?:<p>\s*)?\[gpsies\s*(.*?)\sshow=(.*?)\](?:\s*</p>)?@", array (&$this, 'replace'), $text); 
+	  return $result;
+	}
+	
+	
+	function enquiry ($parms) {
+		
+		$url=GPS_EMBED_API_URL;
+		$url = $url.$parms;
+		//$url = dirname (__FILE__)."/test.xml";
+		$xmlresponse = simplexml_load_file($url)or exit(__("Unable to open URL!"));
+		return $xmlresponse;
+	}
+	
+		
+}
+
+$embediframe = new GPSiesEmbed;
+?>
---
+++ readme.txt
@@ -0,0 +1,79 @@
+=== GPSies Embed ===
+Contributors: flavio alberti
+Tags: google maps, gis, tracks, route, posts, gpssies, 
+Requires at least: 2.5
+Tested up to: 2.7.1
+Stable tag: 0.1
+
+Add GPSies Maps to your posts and pages.(Only for WordPress 2.5+)
+
+== Description ==
+
+Add GPSies Maps to your posts and pages.(Only for WordPress 2.5+)
+This plugin allows you to easily insert GPSies Maps and Track Details into your blog.
+
+This plugin is compliant with GPSies API (http://www.gpsies.com/api/GPSiesAPI_en.pdf)
+and permits to embed the following maps into your blog.
+
+1. Geocoordinate with perimeter
+2. Bounding box (BBOX)
+3. ZIP or city (with countrycode)
+4. Username (optional with countrycode)
+5. Parameter “fileId” (also several)
+6. Search with countrycode and search text and/or username and/or client name
+7. Traverse all tracks in a country
+
+= Display Options: =
+
+	show=details
+	show=summary
+	show=full
+
+= Example for 1: Geocoordinate with perimeter: =
+[gpsies lat=51&lon=10&perimeter=80&limit=20&trackTypes=jogging&filetype=kml&device=Run.GPS show=details]
+
+= Example for 2: Bounding box (BBOX): =
+[gpsies BBOX=10,51,12,53&limit=20&trackTypes=jogging&filetype=kml&device=Run.GPS show=summary]
+
+= Example for 3: ZIP (with countrycode): =
+[gpsies zip=12169&country=DE limit=20&trackTypes=jogging&filetype=kml&device=Run.GPS show=details]
+
+= Example for 3: city (with countrycode): =
+[gpsies city=berlin&country=DE&limit=20&trackTypes=jogging&filetype=kml&device=Run.GPS show=details]
+
+= Example for 4: Username: =
+[gpsies username=GPSies&limit=20&filetype=kml&device=Run.GPS show=details]
+
+= Example for 4: Username (with countrycode): =
+[gpsies username=GPSies&country=DE&limit=20&filetype=kml&device=Run.GPS show=full]
+
+= Example for 5: Parameter „fileId“: =
+[gpsies fileId=haublpggdenoekmb show=details]
+
+= Example for 5: Parameter „fileId“ several: =
+[gpsies fileId=haublpggdenoekmb&fileId=mniajyeljeaaambd show=details]
+
+= Example for 6: Search in country by text and username: =
+[gpsies searchText=test&country=DE&searchUsername=gpsies show=full]
+
+= Example for 6: Search in country by text: =
+[gpsies searchText=test&country=DE show=details]
+
+= Example for 7: traverse all tracks in a country: =
+[gpsies country=DE show=details]
+
+
+= Localization =
+
+If you have translated into your language, please let me know.
+
+== Installation ==
+
+1. Upload the entire `gpsiesEmbed` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Add New Post or Edit Post, and insert the desired [gpsies ...] code
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
