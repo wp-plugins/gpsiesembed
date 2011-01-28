db6cdde18ad226a8d0cb2fe76cb188a513e7b7be
flavio78vi <flavio78vi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-28 19:22:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/gpsiesembed/trunk@338293 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gpsiesEmbed.php
+++ gpsiesEmbed.php
@@ -13,7 +13,7 @@ License:
  Released under the GPL license
   http://www.gnu.org/copyleft/gpl.html
 
-  Copyright 2009 - 2010 flavioalberti (email : flavio@alicubi.net)
+  Copyright 2010 - 2011 flavioalberti (email : flavio@alicubi.net)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
